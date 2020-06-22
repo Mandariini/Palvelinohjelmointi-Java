4c66142b83198f5a118c1c38b7084439adc84e3d
Timo <timo_moilanen@hotmail.com>
2020-06-22 20:29:01 +0300

    muutettu yhteyksien käsittely ja lisätty hylkäystoiminnallisuus.

--- src/main/java/projekti/ConnectionEntityRepository.java
+++
@@ -1,8 +0,0 @@
-
-package projekti;
-/*
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConnectionEntityRepository extends JpaRepository<ConnectionEntity, Long>{
-    
-}
